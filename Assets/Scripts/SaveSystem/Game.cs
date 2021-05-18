2b8ac665c703fb5c70d0c8e05a4d0c6c114726c5
Charly Chavez <charly_chavper@hotmail.com>
2021-05-17 22:20:06 -0500

    Add files via upload

---
+++ Assets/Scripts/SaveSystem/Game.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Game { 
+	
+	public static Game current;
+	public Stats generalStats;
+	public List<Level> world1Levels;
+	//public List<Level> world2Levels;
+	
+	public Game ()
+	{
+		generalStats = new Stats();
+		world1Levels = new List<Level>();
+		//world2Levels = new List<Level>();
+	}
+}
---
+++ Assets/Scripts/SaveSystem/SaveLoad.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; 
+using System.Runtime.Serialization.Formatters.Binary; 
+using System.IO;
+
+public static class SaveLoad
+{
+	public static Game savedGame = new Game();
+	
+	public static void Save()
+	{
+		savedGame = Game.current;
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create (Application.persistentDataPath + "/savedGame.gd");
+		bf.Serialize(file, SaveLoad.savedGame);
+		file.Close();
+	}
+	
+	public static void Load()
+	{
+		if(File.Exists(Application.persistentDataPath + "/savedGame.gd"))
+		{
+			Debug.Log("File found");
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(Application.persistentDataPath + "/savedGame.gd", FileMode.Open);
+			//Debug.Log(Application.persistentDataPath);
+			SaveLoad.savedGame = (Game)bf.Deserialize(file);
+			Game.current = SaveLoad.savedGame;
+			Game.current.world1Levels = SaveLoad.savedGame.world1Levels;
+			file.Close();
+		}
+	}
+}
