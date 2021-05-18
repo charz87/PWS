5d3bd23cd201a319de8bd046c42a5446d6c6ec3b
Charly Chavez <charly_chavper@hotmail.com>
2021-05-17 22:16:00 -0500

    Add files via upload

---
+++ Assets/Scripts/Gameplay/PowerUps/SpiderWeb_PU.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderWeb_PU : PowerUp
+{
+	public GameObject webTrap;
+	
+	void Update ()
+	{
+		transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.Self);
+	}
+	
+	void OnCollisionEnter(Collision vCollision)
+	{
+		if(vCollision.collider.tag == "Enemy")
+		{
+			Collider[] inRange = Physics.OverlapSphere(transform.position, areaOfEffect, myLayer);
+			foreach (Collider col in inRange)
+			{
+				Mushie_AI mushieComp = col.GetComponent<Mushie_AI>();
+				Health mushieHealth = col.GetComponent<Health>();
+				if(mushieComp)
+				{
+					if (mushieHealth.enemyType != Health.typeOfEnemy.giantPurpleMushie && mushieHealth.enemyType != Health.typeOfEnemy.brute_BOSS)
+					{
+						mushieComp.StartCoroutine("webRoutine", effectTime);
+						GameObject myTrapObj = Instantiate(webTrap, col.transform.position, col.transform.rotation) as GameObject;
+						myTrapObj.GetComponent<SpiderWebTrap>().StartCoroutine("timedOutDestroy", effectTime);
+					}
+				}
+			}
+			Destroy(gameObject);
+		}
+	}
+}
